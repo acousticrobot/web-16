9998e9ef29102d44687333fedb2157d94433e6eb
Jonathan Gabel <hello@jonathangabel.com>
2016-01-14 08:39:30 -0500

    refine file utility

--- utilities/art_scraper.rb
+++ utilities/art_scraper.rb
@@ -5,6 +5,7 @@ require 'nokogiri'
 require './lib/file_utility.rb'
 require './lib/art_collection_item.rb'
 
-fu = FileUtility.new(ArtCollectionItem, "utilities/inbox", "utilities/outbox")
-fu.create_collection
+file_utility = FileUtility::FileConverter.new(ArtCollectionItem, "utilities/inbox", "utilities/outbox")
+file_utility.import_batch_files
+file_utility.create_collection
 
